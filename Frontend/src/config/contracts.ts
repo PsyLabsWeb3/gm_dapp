6b540e2bb7b5fcda6ff7751a381c06ec8af7c033
omarigg04 <omar.gaxiola@remtools.mx>
2025-10-13 21:38:37 -0600

    changing contract, launch flag is true now

--- Frontend/src/config/contracts.ts
+++ Frontend/src/config/contracts.ts
@@ -3,7 +3,7 @@
  * Network: Arbitrum Sepolia (Chain ID: 421614)
  */
 
-export const MZCAL_TOKEN_ADDRESS = '0x7B22e1a3272B639AE399405A8f50ddD822B836C6' as const
+export const MZCAL_TOKEN_ADDRESS = '0xBf7C908AF4cE96cae8e0C2F4E493B9cFBD2F51fd' as const
 
 /**
  * Contract Deployer: 0x66299C18c60CE709777Ec79C73b131cE2634f58e
